5328c7bee4f51e953ff9d47a9a9fb1bda3f79c01
Mason Arditi <masonarditi@berkeley.edu>
2025-05-18 17:47:06 -0700

    Revert "migration + seed for projects in profiles table"
    This reverts commit 38fa7efee27c592aad5f7f6a10fa80471259ed82.

--- supabase/migrations/20250528002000_add_projects_to_profiles.sql
+++
@@ -1,5 +0,0 @@
--- 20250528002000_add_projects_to_profiles.sql
-ALTER TABLE "public"."profiles"
-ADD COLUMN IF NOT EXISTS "projects" jsonb[];
-
-COMMENT ON COLUMN "public"."profiles"."projects" IS 'Array of projects for candidate profiles';
